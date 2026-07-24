dd8c1f5cc98d6519d5f038a6be59668f9e38bbb0
bugh4tti <quieroamiabuu@gmail.com>
2026-07-24 08:04:48 -0300

    Enhance DayRooms with SelectionManager integration

--- src/main/java/com/dayrooms/DayRooms.java
+++ src/main/java/com/dayrooms/DayRooms.java
@@ -2,19 +2,24 @@ package com.dayrooms;
 
 import com.dayrooms.commands.DayRoomsCommand;
 import com.dayrooms.gui.MenuListener;
+import com.dayrooms.listeners.SelectionListener;
 import com.dayrooms.managers.RoomManager;
+import com.dayrooms.managers.SelectionManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DayRooms extends JavaPlugin {
 
     private RoomManager roomManager;
+    private SelectionManager selectionManager;
 
     @Override
     public void onEnable() {
         this.roomManager = new RoomManager();
+        this.selectionManager = new SelectionManager();
 
-        getCommand("dayrooms").setExecutor(new DayRoomsCommand(roomManager));
-        getServer().getPluginManager().registerEvents(new MenuListener(), this);
+        getCommand("dayrooms").setExecutor(new DayRoomsCommand(roomManager, selectionManager));
+        getServer().getPluginManager().registerEvents(new MenuListener(roomManager, selectionManager), this);
+        getServer().getPluginManager().registerEvents(new SelectionListener(selectionManager, roomManager), this);
 
         getLogger().info("DayRooms habilitado correctamente.");
     }
@@ -27,4 +32,8 @@ public class DayRooms extends JavaPlugin {
     public RoomManager getRoomManager() {
         return roomManager;
     }
-          }
+
+    public SelectionManager getSelectionManager() {
+        return selectionManager;
+    }
+}
