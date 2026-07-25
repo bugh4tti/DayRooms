0fe207241f18ec91043ef322d558572ec580a326
bugh4tti <quieroamiabuu@gmail.com>
2026-07-25 12:21:17 -0300

    Refactor DayRooms class structure and imports

--- src/main/java/com/dayrooms/DayRooms.java
+++ src/main/java/com/dayrooms/DayRooms.java
@@ -1,154 +1,82 @@
-package com.dayrooms.model;
+package com.dayrooms;
 
-import org.bukkit.Location;
+import com.dayrooms.commands.DayRoomsCommand;
+import com.dayrooms.commands.DayRoomsTabCompleter;
+import com.dayrooms.gui.MenuListener;
+import com.dayrooms.listeners.CommandBlockListener;
+import com.dayrooms.listeners.EffectsListener;
+import com.dayrooms.listeners.PvPListener;
+import com.dayrooms.listeners.RoomEntryListener;
+import com.dayrooms.listeners.RoomListListener;
+import com.dayrooms.listeners.SelectionListener;
+import com.dayrooms.listeners.UtilitiesListener;
+import com.dayrooms.managers.BarrierManager;
+import com.dayrooms.managers.MessageManager;
+import com.dayrooms.managers.RoomManager;
+import com.dayrooms.managers.RoomPersistenceManager;
+import com.dayrooms.managers.SelectionManager;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
+public class DayRooms extends JavaPlugin {
 
-public class Room {
+    private RoomManager roomManager;
+    private SelectionManager selectionManager;
+    private BarrierManager barrierManager;
+    private MessageManager messageManager;
+    private RoomPersistenceManager persistenceManager;
 
-    private final String name;
-    private UUID owner;
-    private final Set<UUID> invitados = new HashSet<>();
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
 
-    private boolean keepInventory = false;
-    private boolean utilidadesHabilitadas = true;
-    private boolean comandosHabilitados = true;
+        this.roomManager = new RoomManager();
+        this.selectionManager = new SelectionManager();
+        this.messageManager = new MessageManager(this);
+        this.barrierManager = new BarrierManager(this, messageManager);
+        this.persistenceManager = new RoomPersistenceManager(this);
 
-    private Location esquina1;
-    private Location esquina2;
-    private boolean esquinasDefinidas = false;
+        persistenceManager.cargarTodas(roomManager);
 
-    private Location barreraEsquina1;
-    private Location barreraEsquina2;
-    private boolean barreraDefinida = false;
+        getCommand("dayrooms").setExecutor(new DayRoomsCommand(roomManager, selectionManager, barrierManager, messageManager, persistenceManager));
+        getCommand("dayrooms").setTabCompleter(new DayRoomsTabCompleter(roomManager));
 
-    private Location teleportLocation;
-    private boolean teleportZoneDefinida = false;
+        getServer().getPluginManager().registerEvents(new MenuListener(roomManager, selectionManager), this);
+        getServer().getPluginManager().registerEvents(new SelectionListener(selectionManager, roomManager), this);
+        getServer().getPluginManager().registerEvents(new EffectsListener(roomManager, selectionManager), this);
+        getServer().getPluginManager().registerEvents(new PvPListener(roomManager, barrierManager, messageManager), this);
+        getServer().getPluginManager().registerEvents(new UtilitiesListener(roomManager, messageManager), this);
+        getServer().getPluginManager().registerEvents(new RoomEntryListener(roomManager, barrierManager, messageManager), this);
+        getServer().getPluginManager().registerEvents(new RoomListListener(roomManager, selectionManager), this);
+        getServer().getPluginManager().registerEvents(new CommandBlockListener(roomManager, messageManager), this);
 
-    private final Map<String, EffectData> efectos = new LinkedHashMap<>();
-
-    public Room(String name, UUID owner) {
-        this.name = name;
-        this.owner = owner;
+        getLogger().info("DayRooms habilitado correctamente.");
     }
 
-    public String getName() {
-        return name;
+    @Override
+    public void onDisable() {
+        if (persistenceManager != null && roomManager != null) {
+            persistenceManager.guardarTodas(roomManager);
+        }
+        getLogger().info("DayRooms deshabilitado.");
     }
 
-    public UUID getOwner() {
-        return owner;
+    public RoomManager getRoomManager() {
+        return roomManager;
     }
 
-    public void setOwner(UUID owner) {
-        this.owner = owner;
+    public SelectionManager getSelectionManager() {
+        return selectionManager;
     }
 
-    public Set<UUID> getInvitados() {
-        return invitados;
+    public BarrierManager getBarrierManager() {
+        return barrierManager;
     }
 
-    public boolean isKeepInventory() {
-        return keepInventory;
+    public MessageManager getMessageManager() {
+        return messageManager;
     }
 
-    public void setKeepInventory(boolean keepInventory) {
-        this.keepInventory = keepInventory;
+    public RoomPersistenceManager getPersistenceManager() {
+        return persistenceManager;
     }
-
-    public boolean isUtilidadesHabilitadas() {
-        return utilidadesHabilitadas;
-    }
-
-    public void setUtilidadesHabilitadas(boolean utilidadesHabilitadas) {
-        this.utilidadesHabilitadas = utilidadesHabilitadas;
-    }
-
-    public boolean isComandosHabilitados() {
-        return comandosHabilitados;
-    }
-
-    public void setComandosHabilitados(boolean comandosHabilitados) {
-        this.comandosHabilitados = comandosHabilitados;
-    }
-
-    public Location getEsquina1() {
-        return esquina1;
-    }
-
-    public void setEsquina1(Location esquina1) {
-        this.esquina1 = esquina1;
-    }
-
-    public Location getEsquina2() {
-        return esquina2;
-    }
-
-    public void setEsquina2(Location esquina2) {
-        this.esquina2 = esquina2;
-    }
-
-    public boolean isEsquinasDefinidas() {
-        return esquinasDefinidas;
-    }
-
-    public void setEsquinasDefinidas(boolean esquinasDefinidas) {
-        this.esquinasDefinidas = esquinasDefinidas;
-    }
-
-    public Location getBarreraEsquina1() {
-        return barreraEsquina1;
-    }
-
-    public void setBarreraEsquina1(Location barreraEsquina1) {
-        this.barreraEsquina1 = barreraEsquina1;
-    }
-
-    public Location getBarreraEsquina2() {
-        return barreraEsquina2;
-    }
-
-    public void setBarreraEsquina2(Location barreraEsquina2) {
-        this.barreraEsquina2 = barreraEsquina2;
-    }
-
-    public boolean isBarreraDefinida() {
-        return barreraDefinida;
-    }
-
-    public void setBarreraDefinida(boolean barreraDefinida) {
-        this.barreraDefinida = barreraDefinida;
-    }
-
-    public Location getTeleportLocation() {
-        return teleportLocation;
-    }
-
-    public void setTeleportLocation(Location teleportLocation) {
-        this.teleportLocation = teleportLocation;
-    }
-
-    public boolean isTeleportZoneDefinida() {
-        return teleportZoneDefinida;
-    }
-
-    public void setTeleportZoneDefinida(boolean teleportZoneDefinida) {
-        this.teleportZoneDefinida = teleportZoneDefinida;
-    }
-
-    public boolean isCompleta() {
-        return esquinasDefinidas && barreraDefinida;
-    }
-
-    public Map<String, EffectData> getEfectos() {
-        return efectos;
-    }
-
-    public EffectData obtenerOCrearEfecto(String key) {
-        return efectos.computeIfAbsent(key, k -> new EffectData(0, 0));
-    }
-    }
+            }
