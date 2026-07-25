8bb440a546a6e39d073c52597c030aa8964c901b
bugh4tti <quieroamiabuu@gmail.com>
2026-07-25 02:22:41 -0300

    Implement MessageManager for configuration handling

--- src/main/java/com/dayrooms/managers/MessageManager.java
+++ src/main/java/com/dayrooms/managers/MessageManager.java
@@ -1,1 +1,22 @@
+package com.dayrooms.managers;
 
+import com.dayrooms.utils.ColorUtils;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MessageManager {
+
+    private final JavaPlugin plugin;
+
+    public MessageManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public String get(String key) {
+        String crudo = plugin.getConfig().getString("mensajes." + key, "");
+        return ColorUtils.traducir(crudo);
+    }
+
+    public void recargar() {
+        plugin.reloadConfig();
+    }
+}
