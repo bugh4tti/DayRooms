81bdf834d582480f05f376cf05cfc6a6d08bc277
bugh4tti <quieroamiabuu@gmail.com>
2026-07-25 12:24:53 -0300

    Fix mapping for FUERZA and update other effects

--- src/main/java/com/dayrooms/utils/EffectTypeMapper.java
+++ src/main/java/com/dayrooms/utils/EffectTypeMapper.java
@@ -6,15 +6,15 @@ public class EffectTypeMapper {
 
     public static PotionEffectType map(String key) {
         return switch (key.toUpperCase()) {
-            case "FUERZA" -> PotionEffectType.STRENGTH;
+            case "FUERZA" -> PotionEffectType.INCREASE_DAMAGE;
             case "VELOCIDAD" -> PotionEffectType.SPEED;
             case "INVISIBILIDAD" -> PotionEffectType.INVISIBILITY;
             case "VISION_NOCTURNA" -> PotionEffectType.NIGHT_VISION;
             case "REGENERACION" -> PotionEffectType.REGENERATION;
-            case "RESISTENCIA" -> PotionEffectType.RESISTANCE;
-            case "SALTO" -> PotionEffectType.JUMP_BOOST;
+            case "RESISTENCIA" -> PotionEffectType.DAMAGE_RESISTANCE;
+            case "SALTO" -> PotionEffectType.JUMP;
             case "PRISA" -> PotionEffectType.FAST_DIGGING;
             default -> null;
         };
     }
-              }
+    }
