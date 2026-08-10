5baec6e50c024f3c5d2c6a8cc56b758e5df3b0c3
bugh4tti <quieroamiabuu@gmail.com>
2026-08-09 23:07:14 -0300

    Add methods to retrieve int and string list from config

--- src/main/java/com/dayrooms/managers/MessageManager.java
+++ src/main/java/com/dayrooms/managers/MessageManager.java
@@ -3,6 +3,8 @@ package com.dayrooms.managers;
 import com.dayrooms.utils.ColorUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+
 public class MessageManager {
 
     private final JavaPlugin plugin;
@@ -14,6 +16,14 @@ public class MessageManager {
     public String get(String key) {
         String crudo = plugin.getConfig().getString("mensajes." + key, "");
         return ColorUtils.traducir(crudo);
+    }
+
+    public int getInt(String key, int valorPorDefecto) {
+        return plugin.getConfig().getInt(key, valorPorDefecto);
+    }
+
+    public List<String> getStringList(String key) {
+        return plugin.getConfig().getStringList(key);
     }
 
     public void recargar() {
